c173876a9b75b88f9876b89ce4741d9984eb9554
cvessen <59048636+cvessen@users.noreply.github.com>
2026-08-28 15:50:20 +0200

    Update testdb.js

--- api/testdb.js
+++ api/testdb.js
@@ -1,5 +1,24 @@
 export default async function handler(req, res) {
-  res.status(200).json({
-    message: "Test endpoint werkt"
-  });
+
+  const response = await fetch(
+    `${process.env.SUPABASE_URL}/rest/v1/Callers`,
+    {
+      method: "POST",
+      headers: {
+        "apikey": process.env.SUPABASE_ANON_KEY,
+        "Authorization": `Bearer ${process.env.SUPABASE_ANON_KEY}`,
+        "Content-Type": "application/json",
+        "Prefer": "return=representation"
+      },
+      body: JSON.stringify([
+        {
+          name: "Coen Test"
+        }
+      ])
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
 }
