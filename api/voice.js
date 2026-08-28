3acd9febed64e854f35ec0d28d5fefacc89ea7c5
cvessen <59048636+cvessen@users.noreply.github.com>
2026-08-28 12:13:34 +0200

    Update voice.js

--- api/voice.js
+++ api/voice.js
@@ -1,12 +1,13 @@
 export default function handler(request, response) {
   const twiml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say language="nl-NL">
-    Goedendag. U spreekt met de digitale assistent van Coen.
-    De verbinding werkt.
-  </Say>
-</Response>`;
-
-  response.setHeader("Content-Type", "text/xml");
+  <Gather
+    input="speech"
+    action="/api/process"
+    method="POST"
+    speechTimeout="auto">
+    <Say language="nl-NL">
+      Goedendag. Wat is uw naam?
+");
   response.status(200).send(twiml);
 }
